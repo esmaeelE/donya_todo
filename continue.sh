a8747c30d1ccbf3e5232b96de40da3d7b982830e
esmaeelE <esmaeelEE@protonmail.com>
2025-01-05 13:26:41 +0330

    Update continue.sh

--- continue.sh
+++ continue.sh
@@ -1,4 +1,23 @@
 # Just enough to run this script for continue development
 
-DONYA_PATH=$HOME/donya/DONYA
+DONYA_PATH=/home/lfs/DONYA
+LFS=$DONYA_PATH
+
 sudo mount /dev/donya/root $DONYA_PATH
+
+# sudo mount /dev/donya/root $DONYA_PATH
+# mount /dev/donya/root $DONYA_PATH
+## above mount source in lvm disk
+
+# user: lfs
+# password: lfs
+
+# show env, why env | grep not work
+# declare -p | grep DONYA
+
+# To continue development run below command
+
+```
+# sudo mount /dev/donya/root /home/lfs/DONYA
+# su - lfs
+```
